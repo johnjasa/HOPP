035f954107a95b9e04fed82695e297069a031acd
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2024-07-12 15:00:57 -0600

    added outline for alkaline cost functions

---
+++ greenheart/simulation/technologies/hydrogen/electrolysis/alkaline_cost_model.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+class AlkalineCosts:
+    def __init__(self,alkaline_design,plant_life_yrs:NotImplementedError):
+        self.plant_life_yrs = plant_life_yrs
+        pass
+    def baseline_alkaline_costs(self):
+        self.stack_replacement_cost_percentage = 0.15
+        uninstalled_capex = []
+        variable_om = []
+        fixed_om = []
+        installation_factor = []
+        indirect_percentage = []
+        feedstock_cost = []
+
+        pass
+    
+    def user_specified_costs(self):
+        pass
+
+
+def alkaline_costs_USD():
+    pass
+
+def alkaline_profast_dictionary():
+    pass
