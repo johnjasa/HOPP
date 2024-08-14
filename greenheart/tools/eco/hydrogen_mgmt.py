8a67a63b1f38138a30e29394bfd6c3f67a98594e
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2024-08-14 16:14:23 -0600

    bug fix from previous commit

--- greenheart/tools/eco/hydrogen_mgmt.py
+++ greenheart/tools/eco/hydrogen_mgmt.py
@@ -122,8 +122,8 @@ def run_h2_transport_compressor(
     ):
         ########## compressor model from Jamie Kee based on HDSAM
         flow_rate_kg_per_hr = max(
-            max(electrolyzer_physics_results["H2_Results"]["Hydrogen Hourly Production [kg/hr]"])
-        )  # kg/hr
+        electrolyzer_physics_results["H2_Results"]["Hydrogen Hourly Production [kg/hr]"]
+    )  # kg/hr
         
         if "n_compressors" in greenheart_config["h2_transport_compressor"].keys():
             number_of_compressors = greenheart_config["h2_transport_compressor"]["n_compressors"]
@@ -197,7 +197,7 @@ def run_h2_transport_pipe(
     # prepare inputs
     export_pipe_length = orbit_config["site"]["distance_to_landfall"]  # Length [km]
     mass_flow_rate = max(
-        max(electrolyzer_physics_results["H2_Results"]["Hydrogen Hourly Production [kg/hr]"])
+        electrolyzer_physics_results["H2_Results"]["Hydrogen Hourly Production [kg/hr]"]
     ) * (
         (1.0 / 60.0) ** 2
     )  # from [kg/hr] to mass flow rate in [kg/s] assuming 300 MW -> 1.5 kg/s
