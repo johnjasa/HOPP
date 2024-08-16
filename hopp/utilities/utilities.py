239dcc4f41576c50cc6b974c3cd76695c2c1286c
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2024-08-16 11:10:46 -0600

    removed unused/buggy loaders from hopp utilities

--- hopp/utilities/utilities.py
+++ hopp/utilities/utilities.py
@@ -20,53 +20,6 @@ class Loader(yaml.SafeLoader):
 
 Loader.add_constructor('!include', Loader.include)
 
-
-class SmartLoader(yaml.Loader):
-
-    def __init__(self, stream):
-
-        self._root = os.path.split(stream.name)[0]
-
-        super().__init__(stream)
-
-    def include(self, node):
-        if os.path.split(node.value)[0] == "":
-            filename = os.path.join(self._root, self.construct_scalar(node))
-        else:
-            if os.path.isfile(node.value):
-                filename = self.construct_scalar(node)
-
-        with open(filename, 'r') as f:
-            return yaml.load(f, self.__class__)
-
-
-SmartLoader.add_constructor('!include', SmartLoader.include)
-
-class SuperSmartLoader(yaml.Loader):
-
-    def __init__(self, stream):
-
-        self._root = os.path.split(stream.name)[0]
-
-        super().__init__(stream)
-
-    def include(self, node):
-        if os.path.split(node.value)[0] == "":
-            filename = os.path.join(self._root, self.construct_scalar(node))
-        else:
-            if os.path.isfile(node.value):
-                filename = self.construct_scalar(node)
-            else:
-                # filename = os.path.join(INPUT_DIR,self.construct_scalar(node))
-                if os.path.isfile(os.path.join(INPUT_DIR,self.construct_scalar(node))):
-                    filename = os.path.join(INPUT_DIR,self.construct_scalar(node))
-        # print(filename)
-        with open(filename, 'r') as f:
-            return yaml.load(f, self.__class__)
-
-
-SuperSmartLoader.add_constructor('!include', SuperSmartLoader.include)
-
 def load_yaml(filename, loader=Loader) -> dict:
     if isinstance(filename, dict):
         return filename  # filename already yaml dict
