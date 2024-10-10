3bb2b2d75aafd8d26a8f3134f2a71ced20558790
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2024-10-10 16:31:07 -0600

    updated reference plant example 1 for dri - i think

--- examples/reference_plants/01-onshore-steel-mn/onshore-steel-mn.py
+++ examples/reference_plants/01-onshore-steel-mn/onshore-steel-mn.py
@@ -1,6 +1,6 @@
 # general imports
 import os
-
+import dill
 # # yaml imports
 import yaml
 from yamlinclude import YamlIncludeConstructor
@@ -24,37 +24,65 @@ from greenheart.tools.optimization.gc_run_greenheart import run_greenheart
 # run the stuff
 if __name__ == "__main__":
     # load inputs as needed
-    turbine_model = "lbw_6MW"
+    turbine_model = "ATB2024_6MW_170RD_floris_turbine"
     filename_turbine_config = "./input/turbines/" + turbine_model + ".yaml"
     filename_floris_config = "./input/floris/floris_input_lbw_6MW.yaml"
     filename_hopp_config = "./input/plant/hopp_config_mn.yaml"
-    filename_greenheart_config = "./input/plant/greenheart_config_onshore_mn.yaml"
+    # filename_greenheart_config = "./input/plant/greenheart_config_onshore_mn.yaml"
+    policy_scenarios = [1,7]
+    design_scenarios = [9,11]
+    filename_greenheart_config = "./input/plant/greenheart_config_dri_mn.yaml"
 
-    config = GreenHeartSimulationConfig(
-        filename_hopp_config,
-        filename_greenheart_config,
-        filename_turbine_config,
-        filename_floris_config,
-        verbose=True,
-        show_plots=False,
-        save_plots=True,
-        use_profast=True,
-        post_processing=True,
-        incentive_option=1,
-        plant_design_scenario=9,
-        output_level=7,
-    )
+    for incentive_num in policy_scenarios:
+        for pdscenario in design_scenarios:
+    
+    # incentive_num = 1 #1 and 7
+    # pdscenario = 11 #9 and 11
 
-    # for analysis
-    prob, config = run_greenheart(config, run_only=True)
+            output_dir = os.path.join(os.path.dirname(__file__),"results-40percent_fullsize_CF1_-policy{}-design{}".format(incentive_num,pdscenario))
+            # output_dir = os.path.join(os.path.dirname(__file__),"CF1_results-fullsize-policy{}-design{}".format(incentive_num,pdscenario))
+            os.makedirs(output_dir,exist_ok=True)
+            config = GreenHeartSimulationConfig(
+                filename_hopp_config,
+                filename_greenheart_config,
+                filename_turbine_config,
+                filename_floris_config,
+                verbose=True,
+                show_plots=False,
+                save_plots=False,
+                use_profast=True,
+                post_processing=True,
+                incentive_option=incentive_num,
+                plant_design_scenario=pdscenario,
+                output_level=8,
+                output_dir=output_dir,
+            )
+            
+
+            # for analysis
+            res = run_simulation(config)
+            attr_of_interest = ["electrolyzer_physics_results","profast_lcoh","capex_breakdown","opex_breakdown_annual","dri_capacity","dri_costs","dri_finance","greenheart_config","annual_energy_breakdown","hourly_energy_breakdown"]
+            attr_of_interest +=["h2_storage_max_fill_rate_kg_hr","h2_storage_capacity_kg","hydrogen_storage_state_of_charge_kg"]
+            print("output dir")
+            print(output_dir)
+            for thing in attr_of_interest:
+                print("saving {}".format(thing))
+
+                filename = os.path.join(output_dir,thing)
+                data = res.__getattribute__(thing)
+                with open(filename,"wb") as f:
+                    dill.dump(data,f)
+
+    # prob, config = run_greenheart(config, run_only=True)
 
     # for optimization
     # prob, config = run_greenheart(config, run_only=False)
     
-    lcoe = prob.get_val("lcoe", units="USD/(MW*h)")
-    lcoh = prob.get_val("lcoh", units="USD/kg")
-    lcos = prob.get_val("lcos", units="USD/t")
+    # lcoe = prob.get_val("lcoe", units="USD/(MW*h)")
+    # lcoh = prob.get_val("lcoh", units="USD/kg")
+    # lcos = prob.get_val("lcodri", units="USD/t")
 
-    print("LCOE: ", lcoe, "[$/MWh]")
-    print("LCOH: ", lcoh, "[$/kg]")
-    print("LCOS: ", lcos, "[$/metric-tonne]")
+    # print("LCOE: ", lcoe, "[$/MWh]")
+    # print("LCOH: ", lcoh, "[$/kg]")
+    # print("LCODRI: ", lcos, "[$/metric-tonne]")
+    print("Done")
